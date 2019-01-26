e1e7d8502be059a402c4deb48fbd3232335dd9ab
cc <cc>
2019-01-26 04:00:38 +0000

    add ssh

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM ubuntu:18.04
 WORKDIR /app
 
-RUN apt update && apt install -y wget tmux slime
+RUN apt update && apt install -y wget tmux slime openssh-server
 RUN apt-get purge --auto-remove && rm -rf /tmp/* /var/lib/apt/lists/* && apt-get -s clean
 RUN wget https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz
 RUN tar -xvf gotty_linux_amd64.tar.gz &&  rm gotty_linux_amd64.tar.gz
 
 ENV TERM=xterm
-CMD ["/app/gotty","-w","emacs"]
+CMD ["/app/startup.sh"]
---
+++ startup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -e
+echo -e "\n------------------ add user ------------------"
+
+# default username and userpassword
+name=bear
+password=bear
+
+# set user name
+if [ $USER_NAME ];then
+        name=$USER_NAME
+        echo "USER_NAME = $USER_NAME"
+else
+        echo "USER_NAME = $name"
+fi
+
+# set user password
+if [ $USER_PASSWORD ];then
+        password=$USER_PASSWORD
+        echo "USER_PASSWORD = $USER_PASSWORD"
+else
+        echo "USER_PASSWORD = $password"
+fi
+
+# create user
+echo "create user.."
+if [ $(getent passwd $name) ] ; then
+    echo "$name exists!"
+else
+    useradd -m $name
+    adduser $name sudo
+    echo -e "${password}\n${password}" | passwd $name
+fi
+
+# login with no-root user
+/bin/su -l $name -c '/app/gotty -w emacs &'
+
+
+
+
